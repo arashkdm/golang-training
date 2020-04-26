fbe934e2a678911122736dc256517cb42b0282c5
archioux <arash.khoddami@gmail.com>
2020-04-26 18:58:48 +0430

    learning golang

--- 010-short-declaraion-operator/main.go
+++ 010-short-declaraion-operator/main.go
@@ -4,17 +4,22 @@ import "fmt"
 
 var a = 50
 var b int
+var c string = `James said, "hahahaha"`
 
 func main() {
 	x := 42
 	fmt.Println(x)
+	fmt.Printf("%T\n", x)
 	x = 99
 	fmt.Println(x)
 	y := 100 + 42
 	fmt.Println(y)
 	z := "Bond"
 	fmt.Println(z)
+	fmt.Printf("%T\n", z)
 	fmt.Println(a)
+	// d := 80 error because is unused
+	y = 94 // won't error because is only a change
 
 	foo()
 }
@@ -22,4 +27,6 @@ func main() {
 func foo() {
 	fmt.Println("again: ", a)
 	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Printf("%T\n", c)
 }
